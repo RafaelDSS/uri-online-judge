cccd050d3024e0959c3c8fccb10a5875d4f02791
rafaeldss <rafaeldss199916@gmail.com>
2018-09-18 18:04:19 -0300

    Desafio 1008

---
+++ iniciante/uri-1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+numero = int(input())
+horas = int(input())
+salario = float(input())
+
+SALARY = salario * horas
+
+print('NUMBER = %d' % numero)
+print('SALARY = U$ %.2f' % SALARY)
