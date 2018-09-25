951609e8e1b0d935fc39d6c46be0a8047ce3cd00
rafaeldss <rafaeldss199916@gmail.com>
2018-09-25 12:22:09 -0300

    Desafio 1564

---
+++ iniciante/uri-1564.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+while True:
+    try:
+        n = input()
+    except:
+        break
+    
+    if n == '0':
+        print('vai ter copa!')
+    else: 
+        print('vai ter duas!')
