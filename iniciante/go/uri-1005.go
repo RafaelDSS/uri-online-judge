e96091023b6bc3c886c182c6fccfec480438c27d
rafaeldss <rafaeldss199916@gmail.com>
2018-12-17 22:28:59 -0300

    Desafio 1005 em Go

---
+++ iniciante/go/uri-1005.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a, b, media float64
+
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+
+	media = ((a * 3.5) + (b * 7.5)) / 11
+
+	fmt.Printf("MEDIA = %.5f\n", media)
+
+
+}
