b42a70e8e2a17c6274ca0383d688998960d36152
rafaeldss <rafaeldss199916@gmail.com>
2021-07-15 16:00:39 -0300

    Correção desafio 1051

--- iniciante/python/uri-1051.py
+++ iniciante/python/uri-1051.py
@@ -1,4 +1,14 @@
-value = float(input())
+salario = float(input())
 
-if value < 2000.00:
-    print('Inseto')
+imposto = 0
+
+if salario < 2000.00:
+    print('Isento')
+else:
+    if salario > 2000.00 and salario <= 3000.00:
+        imposto = (salario - 2000.0) * 0.08
+    elif salario > 3000.00 and salario <= 4500.00:
+        imposto = (1000.00 * 0.08) + ((salario - 3000.0) * 0.18)
+    else:
+        imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + (salario - 4500.0) * 0.28
+    print(f"R$ {imposto:.2f}")
