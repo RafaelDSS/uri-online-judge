46cc73616fc224e657097fe332340b59eccf9b7b
Rafael De Souza Santos <RafaelDSS199916@gmail.com>
2022-01-09 16:37:39 -0300

    Desafio 2742 em PostgreSQL

---
+++ sql/uri-2742.sql
@@ -0,0 +1,5 @@
+SELECT life_registry.name, ROUND((life_registry.omega * 1.618), 3) FROM dimensions
+    INNER JOIN life_registry
+    ON life_registry.dimensions_id = dimensions.id
+    WHERE dimensions.name IN ('C875', 'C774') AND life_registry.name LIKE 'Richard%'
+    ORDER BY life_registry.omega ASC;
