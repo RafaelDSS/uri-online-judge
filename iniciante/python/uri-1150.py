fe3f7e8b3e9be206d4430b6884b26ae3ca0787fd
rafaeldss <rafaeldss199916@gmail.com>
2025-02-10 23:13:11 -0300

    Desafio 1150 em Python

---
+++ iniciante/python/uri-1150.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+x = int(input())
+z = int(input())
+
+while z <= x:
+    z = z = int(input())
+
+soma = x
+qt = 1
+while soma <= z:
+    soma += x + qt
+    qt += 1
+print(qt)
